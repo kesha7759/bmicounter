df66e750ed8174f8d9ec819f8728b71c8d4c45b7
kesha7759 <ksenia.bel9eva@yandex.ru>
2020-07-10 15:42:29 +0300

    текущая версия проекта

---
+++ BMIcounter/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMIcounter
+{
+    class Counter
+    {
+        private double height;
+        public int Weight { get; set; }
+        
+        public double Height 
+        {
+            get => height;
+            set => height = value/100;
+        }
+        public Counter (int weight, int height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+        public double Score()
+        {
+            return Weight/(Height*Height);   
+        }
+    }
+}
---
+++ BMIcounter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BMIcounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter newCounter = new Counter(60, 165);
+            double score = newCounter.Score();
+            Console.WriteLine(score);
+            Console.ReadLine();
+        }
+    }
+}
